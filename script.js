dedbc6709054b738122bcb86374680252e9a39db
Starz1uz <148357539+Starz1uz@users.noreply.github.com>
2024-01-29 16:34:16 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,42 @@
+const img = document.querySelector('#scroll_img')
+const btns = document.querySelectorAll('#titan, #blue, #white, #black')
+
+
+const images = {
+    titan: "./img_or_Icons/titan.jpeg",
+    blue: "./img_or_Icons/blue.jpeg",
+    white: "./img_or_Icons/white.jpeg",
+    black: "./img_or_Icons/black.jpeg",
+}
+
+btns.forEach(btn => {
+    btn.onclick = () => {
+        let color = btn.innerHTML
+        img.setAttribute('src', images[color])
+    }
+})
+
+let find = document.getElementsByClassName("accordion");
+
+let k
+
+for (k = 0; k < find.length; k++) {
+  find[k].addEventListener("click", function() {
+    this.classList.toggle("active");
+    let panel = this.nextElementSibling;
+    
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
+
+// let boxes = document.querySelectorAll('.boxes')
+
+// boxes.forEach(box => {
+//     box.onmouseenter = () => {
+//         box.classList.add('active')
+//     }
+// })
